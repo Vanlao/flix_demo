504787f9cbacee9f79bb27f1b557e2d21d5530c2
Van Lao <laominhvan1994@gmail.com>
2018-11-06 08:42:02 -0800

    Initial Commit

---
+++ flix_demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix_demo
+//
+//  Created by Tu Pham on 11/6/18.
+//  Copyright © 2018 Van Lao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
